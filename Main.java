a05b845ca1d120fd551e3a4085baa35c9f22a8a0
Jaime Silva <jaaiimeee91@gmail.com>
2026-04-15 21:20:04 -0300

    Renomeando para Main.java e testando

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercicio1001 { // No Beecrowd, a classe SEMPRE deve se chamar Main
+public class Main { // No Beecrowd, a classe SEMPRE deve se chamar Main
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
